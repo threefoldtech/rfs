87fa7815f4375a0491321cf4a760b39c0fc5799d
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-02-26 08:17:15 +0200

    WIP: experment with capnp

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate libc;
 extern crate log;
 extern crate capnp;
 extern crate simple_logger;
+extern crate sqlite;
 
 use std::ffi;
 use std::path;
@@ -12,12 +13,30 @@ mod fs;
 pub mod schema_capnp;
 
 fn main() {
-    simple_logger::init_with_level(log::Level::Debug).unwrap();
-    let p = path::Path::new("/tmp/mnt");
+    let con = sqlite::open("/tmp/flistdb.sqlite3").unwrap();
+    let mut statement = con.prepare("select key, value from entries;").unwrap();
+    while let sqlite::State::Row = statement.next().unwrap() {
+        let key: String = statement.read(0).unwrap();
+        let mut value: Vec<u8> = statement.read(1).unwrap();
+        
+        capnp::serialize::read_message(&mut value, capnp::message::ReaderOptions::default())
+        let msg = capnp::serialize_packed::read_message(
+            &mut value,
+            capnp::message::ReaderOptions::default(),
+        )
+        .unwrap();
 
-    let o: [&ffi::OsStr; 0] = [];
-
-    let f = fs::Filesystem::new();
-
-    fuse::mount(f, &p, &o).unwrap();
+        println!("Key {} Value {}", key, value.len());
+    }
 }
+
+// fn main() {
+//     simple_logger::init_with_level(log::Level::Debug).unwrap();
+//     let p = path::Path::new("/tmp/mnt");
+
+//     let o: [&ffi::OsStr; 0] = [];
+
+//     let f = fs::Filesystem::new();
+
+//     fuse::mount(f, &p, &o).unwrap();
+// }
