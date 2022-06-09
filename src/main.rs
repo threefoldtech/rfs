4ed6e73f0b18caf00a186597af5dccfd4f062853
Muhamad Azamy <muhamad@incubaid.com>
2022-06-09 10:35:01 +0200

    Fix build against musl

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ pub mod schema_capnp {
     include!(concat!(env!("OUT_DIR"), "/schema_capnp.rs"));
 }
 
+const GIT_VERSION: &str =
+    git_version::git_version!(args = ["--tags", "--always", "--dirty=-modified"]);
+
 struct Options {
     hub: String,
     meta: String,
@@ -24,7 +27,7 @@ struct Options {
 
 fn main() -> Result<()> {
     let matches = App::new("Mount flists")
-        .version("0.2")
+        .version(GIT_VERSION)
         .author("Threefold Tech")
         .arg(
             Arg::with_name("debug")
