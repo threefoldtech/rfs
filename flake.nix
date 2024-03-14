fc97314291fdca29f374ec819cf6468ea0a0ce4d
Stefan Junker <1181362+steveej@users.noreply.github.com>
2024-03-14 14:05:24 +0100

    feat: add initial nix flake to package the rfs binary (#40)
    * feat: add initial nix flake to package the rfs binary
    
    * fix(build.rs): add fallback git_version
    
    this is required for the nix package to work, because flakes filter out
    `.git` by default for the sake of a deterministic src input.
    
    * feat: add .envrc for direnv support
    
    * feat(flake): add devShell using crane
    
    also adding rust-analyzer as it's a common tool for rust development
    work.
    
    * fix(flake/devShell): add RUST_SRC_PATH for rust-analyzer to find it
    
    * chore: format flake.nix

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+  }:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.x86_64-linux
+      flake-utils.lib.system.aarch64-linux
+      flake-utils.lib.system.aarch64-darwin
+    ] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      craneLib = crane.lib.${system};
+    in {
+      devShells.default = craneLib.devShell {
+        packages = [
+          pkgs.rust-analyzer
+        ];
+
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+      };
+      packages.default = craneLib.buildPackage {
+        src = self;
+
+        # 2024-03-07 failing test:
+        # > thread 'test::pack_unpack' has overflowed its stack
+        # > fatal runtime error: stack overflow
+        # > error: test failed, to rerun pass `--lib`
+        #
+        # appearantly needs `RUST_MIN_STACK: 8388608` according to https://github.com/threefoldtech/rfs/blob/eae5186cc6b0f8704f3e4715d2e3644f1f3baa2c/.github/workflows/tests.yaml#L25C1-L25C34
+        doCheck = false;
+
+        cargoExtraArgs = "--bin rfs --features=build-binary";
+
+        nativeBuildInputs = [
+          pkgs.perl
+          pkgs.pkg-config
+        ];
+
+        buildInputs = [
+          pkgs.openssl
+          pkgs.openssl.dev
+        ];
+      };
+    });
+}
