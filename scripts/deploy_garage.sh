7d142b05c06aed33ae709b1e91a00f8915039736
rawdaGastan <rody.rawda.rr@gmail.com>
2024-06-13 16:08:34 +0300

    remove public ips and use mycelium, serve using caddy

--- scripts/deploy_garage.sh
+++ scripts/deploy_garage.sh
@@ -8,25 +8,16 @@ then
     exit 64
 fi
 
-# Deploy a vm for garage server with mycelium and public IP for s3 server TODO: mycelium and remove public IP
+# Deploy a vm for garage server with mycelium for s3 server
 
-tfcmd deploy vm --name s3_server --ssh ~/.ssh/id_rsa.pub --cpu 8 --memory 16 --disk 50 --ipv4 --rootfs 10
+tfcmd deploy vm --name s3_server --ssh ~/.ssh/id_rsa.pub --cpu 8 --memory 16 --disk 50 --rootfs 10
 sleep 6 # wait deployment
 OUTPUT=$(tfcmd get vm s3_server 2>&1 | tail -n +3 | tr { '\n' | tr , '\n' | tr } '\n')
 MYCELIUM_IP=$(echo "$OUTPUT" | grep -Eo '"mycelium_ip"[^,]*' | awk  -F'"' '{print $4}')
-PUBLIC_IP=$(echo "$OUTPUT" | grep -Eo '"computedip"[^,]*' | awk  -F'"' '{print $4}' | cut -d/ -f1-1)
 
-# Deploy a name gateway to expose a domain for garage web
+# Expose S3 server over mycelium IP 
 
-tfcmd deploy gateway name -n flist.$DOMAIN --backends http://$PUBLIC_IP:3902
-tfcmd deploy gateway name -n blobs.$DOMAIN --backends http://$PUBLIC_IP:3902
-sleep 6 # wait deployment
-OUTPUT=$(tfcmd get gateway name $DOMAIN 2>&1 | tail -n +3 | tr { '\n' | tr , '\n' | tr } '\n')
-FQDN=$(echo "$OUTPUT" | grep -Eo '"FQDN"[^,]*' | awk  -F'"' '{print $4}')
-
-# Expose S3 server over public IP (mycelium not suppoerted yet) (garage is used) TODO: mycelium and remove public IP
-
-ssh root@$PUBLIC_IP "
+ssh root@$MYCELIUM_IP "
 wget https://garagehq.deuxfleurs.fr/_releases/v1.0.0/x86_64-unknown-linux-musl/garage
 chmod +x garage
 mv garage /usr/local/bin
@@ -49,7 +40,7 @@ root_domain = '.s3.garage.localhost'
 
 [s3_web]
 bind_addr = '[::]:3902'
-root_domain = '.$FQDN'
+root_domain = ".web.garage.localhost"
 index = 'index.html'
 
 [k2v_api]
--- scripts/manage_buckets.sh
+++ scripts/manage_buckets.sh
@@ -2,17 +2,17 @@
 
 set -ex 
 
-if [ -z ${PUBLIC_IP+x} ]
+if [ -z ${MYCELIUM_IP+x} ]
 then
-    echo 'Error! $PUBLIC_IP is required.'
+    echo 'Error! $MYCELIUM_IP is required.'
     exit 64
 fi
 
 # Create flist bucket and blobs bucket for rfs store and allow web for both
 
-NODE_ID=$(ssh root@$PUBLIC_IP "garage status | awk 'NR==3{print \$1}'")
+NODE_ID=$(ssh root@$MYCELIUM_IP "garage status | awk 'NR==3{print \$1}'")
 
-ssh root@$PUBLIC_IP "
+ssh root@$MYCELIUM_IP "
 garage layout assign -z dc1 -c 1G $NODE_ID
 garage layout apply --version 1
 garage bucket create blobs
@@ -25,7 +25,7 @@ garage bucket list
 # We need to generate a key allowing read and write permissions
 # This allow us to upload different files over the s3 server bucket
 
-WRITE_KEY_INFO=$(ssh root@$PUBLIC_IP "garage key create write-rfs-key | awk 'NR==2{print \$3}NR==3{print \$3}'")
+WRITE_KEY_INFO=$(ssh root@$MYCELIUM_IP "garage key create write-rfs-key | awk 'NR==2{print \$3}NR==3{print \$3}'")
 WRITE_KEY_ID=$(echo $KEY_INFO | awk '{print $1}')
 WRITE_KEY_SECRET=$(echo $KEY_INFO | awk '{print $2}')
 
@@ -33,11 +33,11 @@ WRITE_KEY_SECRET=$(echo $KEY_INFO | awk '{print $2}')
 # This allow us to only download different files over the s3 server bucket
 # After generating the flist it should be updated to include the read key only
 
-READ_KEY_INFO=$(ssh root@$PUBLIC_IP "garage key create read-rfs-key | awk 'NR==2{print \$3}NR==3{print \$3}'")
+READ_KEY_INFO=$(ssh root@$MYCELIUM_IP "garage key create read-rfs-key | awk 'NR==2{print \$3}NR==3{print \$3}'")
 READ_KEY_ID=$(echo $KEY_INFO | awk '{print $1}')
 READ_KEY_SECRET=$(echo $KEY_INFO | awk '{print $2}')
 
-ssh root@$PUBLIC_IP "
+ssh root@$MYCELIUM_IP "
 garage bucket allow \
   --read \
   --write \
