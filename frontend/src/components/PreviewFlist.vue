dc14b9cb10693771ecad63f8f33c3d7374ca5cfa
Salma Elsoly <salmaelsoly@gmail.com>
2024-08-22 12:36:12 +0300

    feat: add more fields in preview page
    Co-authored-by: nabil.salah203@gmail.com <nabil.salah203@gmail.com>

--- frontend/src/components/PreviewFlist.vue
+++ frontend/src/components/PreviewFlist.vue
@@ -14,30 +14,54 @@
       <v-container class="m-0 pa-0">
         <v-row>
           <div>
-            <h2 class="text-h4">{{
+            <h2 class="text-h4 mb-3">{{
               id
               }}</h2>
             <p>This Flist was created by <v-chip color="#1aa18f" label>{{ username }} </v-chip> </p>
           </div>
         </v-row>
         <v-row class="d-flex flex-column">
-            <h3 class="text-h5">Source file</h3>
+            <h3 class="text-subtitle-1 text-grey-darken-2">Source file</h3>
             <v-text-field rounded="20" variant="outlined" density="compact" readonly class="text-grey-darken-1 mr-0">
               {{ url }}
               <template #append>
-        <v-btn
-            color="#1aa18f"
-            value="Copy"
-            class="Btn"
-            @click="copyLink(url)">
-        Copy
-        </v-btn>
+                <v-btn
+                    color="#1aa18f"
+                    value="Copy"
+                    class="Btn"
+                    @click="copyLink(url)">
+                Copy
+                </v-btn>
+              </template>
+            </v-text-field>
+        </v-row>
+        <v-row class="d-flex flex-column">
+            <h3 class="text-subtitle-1 text-grey-darken-2">Archive Checksum (MD5)</h3>
+            <v-text-field rounded="20" variant="outlined" density="compact" readonly class="text-grey-darken-1 mr-0">
+              checksum
+              <template #append>
+                <v-btn
+                    color="#1aa18f"
+                    value="Copy"
+                    class="Btn"
+                    @click="copyLink(url)">
+                Copy
+                </v-btn>
+              </template>
+            </v-text-field>
+        </v-row>
+        <v-row class="d-flex flex-column">
+            <h3 class="text-subtitle-1 text-grey-darken-2">Metadata</h3>
+            <v-text-field rounded="20" variant="outlined" density="compact" readonly class="text-grey-darken-1 mr-0" width="98.5%">
+              {{ url }}
+              <template #prepend-inner>
+       <v-chip color="#1aa18f" label class ="chip">Backend (default)</v-chip>
     </template>
             </v-text-field>
         </v-row>
           <v-row class="d-flex flex-column">
-            <h3 class="text-h5">Content</h3>
-            <v-textarea :model-value="showContent" variant="outlined" readonly class="text-grey-darken-1" auto-grow width="98.5%">
+            <h3 class="text-subtitle-1 text-grey-darken-2">Content</h3>
+            <v-textarea :model-value="showContent" variant="outlined" readonly rows="1" :class= "linkDecoration" class="text-grey-darken-1"  auto-grow width="98.5%" @click="contentShow()">
             </v-textarea>
         </v-row>
       </v-container>
@@ -66,11 +90,17 @@ const content = ref<string[]>([]);
 const url = baseURL + "/flists" + "/" + username.value + "/" + id.value
 
 const showContent = ref<string>()
+const linkDecoration = ref<string>("text-as-anchor")
+const contentShow = () => {
+  showContent.value = content.value.join("\n")
+  linkDecoration.value = ""
+}
 onMounted(async () => {
   try {
     content.value = (await api.get(url)).data;
     content.value = content.value.slice(1)
-    showContent.value = content.value.join("\n")
+    showContent.value = "show content on click"
+    //showContent.value = content.value.join("\n")
 
   } catch (error: any) {
     console.error("Failed to fetch flists", error);
@@ -84,8 +114,22 @@ onMounted(async () => {
 .Btn{
   position: relative;
   left: -18px;
-  height: 50px;
+  height: 40px;
   width: 110px;
   margin-left:0px;
+}
+
+.chip{
+  height: 40px;
+  position: relative;
+  left: -11px;
+}
+
+.text-as-anchor {
+  color: #42A5F5; 
+  cursor: pointer;
+}
+.text-as-anchor:hover {
+  text-decoration: underline;
 }
 </style>
